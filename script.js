59befffb9be9008fbfe8970a1c1deebdfd242f36
V0NASHL <136652079+V0NASHL@users.noreply.github.com>
2025-01-16 15:31:41 -0500

    add functions to add and remove books from table

--- script.js
+++ script.js
@@ -5,9 +5,44 @@ function Book(title, author, pages, read){
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.bookInfo = function(){
-        console.log(this.title, this.author, this.pages, this.read)
-    };
 };
 
-function addBookToLibrary(){};
+function render(){
+    let table = document.getElementById("tableData").getElementsByTagName("tbody")[0];
+    table.innerHTML = ""; 
+
+    for (let i = 0; i < myLibrary.length; i++) {
+        let bookInfo = myLibrary[i];
+        let row = document.createElement("tr");
+
+        row.innerHTML = `
+            <td>${bookInfo.title}</td>
+            <td>${bookInfo.author}</td>
+            <td>${bookInfo.pages}</td>
+            <td>${bookInfo.read}</td>
+            <td><button class="remove" onclick="remove(${i})">Remove</button></td>
+            `;
+
+        table.appendChild(row);
+
+    }
+};
+
+function remove(index){
+    myLibrary.splice(index, 1);
+    render();
+};
+
+
+function addBookToLibrary(){
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let read = document.getElementById("status").value
+
+    let newBook = new Book(title, author, pages, read);
+
+    myLibrary.push(newBook);
+    render();
+
+};
